7a35a67ea2d5e8857ef829a68c9fcd19aeaf036b
Mayank Patke <mayank.patke@gmail.com>
2014-01-18 03:37:38 -0500

    Updated save

--- chromescript.js
+++ chromescript.js
@@ -1,7 +1,14 @@
 window.onload = function() {
 
+	var currentFile;
+	var isSaved = true;
+
 	var filePicker = document.getElementById("FilePicker");
 	var codeBox = document.getElementById("scriptingspace");
+
+	codeBox.addEventListener('onkeydown', textChanged);
+
+	populateFiles();
 
 	var newButton = document.getElementById("new");
 	var saveButton = document.getElementById("save");
@@ -15,22 +22,27 @@ window.onload = function() {
 	importButton.addEventListener('click', importFile);
 	runButton.addEventListener('click', run);
 
+	function textChanged() {
+		isSaved = false;
+	}
+
 	function newFile() {
 
 	}
 
 	function save() {
-
+		isSaved = true;
 	}
 
 	function saveAs() {
 		var name = prompt("Save as: ", "Enter script name");
 		chrome.storage.local.get(name, function(items) {
-			if (items[name] !== undefined || confirm("Script already exists. Overwrite?")) {
+			if (items[name] === undefined || confirm("Script already exists. Overwrite?")) {
 				var item = {};
 				item[name] = codeBox.value;
 				chrome.storage.local.set(item);
 				populateFiles();
+				isSaved = true;
 			}
 		});
 	}
@@ -64,6 +76,11 @@ window.onload = function() {
 				addFile(key);
 			}
 		});
+	}
+
+	function closeFile() {
+		if (isSaved) return;
+		prompt("")
 	}
 
 };
