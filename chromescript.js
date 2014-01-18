9eba1ac9f40b88f816fe33381bdc350b6558957c
Mayank Patke <mayank.patke@gmail.com>
2014-01-18 01:17:21 -0500

    Revert "Updated JS to match HTML"
    This reverts commit e56064b3b032bfe5d5e4cd304217a0127735fd42.

--- chromescript.js
+++ chromescript.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	var codeBox = document.getElementById("scriptingspace");
-	var runButton = document.getElementById("run");
+	var runButton = document.getElementById("submit");
 
 	runButton.onclick = function() {
 		chrome.tabs.executeScript({
