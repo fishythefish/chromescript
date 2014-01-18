6e97606776d54674108a1d5c8f5f5e088452be1b
Mayank Patke <mayank.patke@gmail.com>
2014-01-18 16:47:01 -0500

    whats going on

--- devtools.js
+++ devtools.js
@@ -1,3 +1,16 @@
 chrome.devtools.panels.create("ChromeScript", "icon/icon16.png", "chromescript.html", function(panel) {
 
 });
+
+var backgroundPageConnection = chrome.runtime.connect({
+	name: "devtools-page"
+});
+
+backgroundPageConnection.onMessage.addListener(function(message) {
+
+});
+
+chrome.runtime.sendMessage({
+	tabId: chrome.devtools.inspectedWindow.tabId,
+	codeToInject: ""
+});
